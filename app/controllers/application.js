f6c8e014b2e9214806838502bb9f6251d57e7d0f
matthew philyaw <matthew.philyaw@gmail.com>
2014-03-25 07:25:15 -0400

    removed nav hash from the index controller and removed the default property out of the nav hash on the applicatoin controller. No need for a default route right now.

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -4,21 +4,18 @@ export default Ember.Controller.extend({
             active: true,
             name: 'Index', 
             route: 'index', 
-            default: true,
             hidden: true
         }),
         Em.Object.create({ 
-            active: true,
+            active: false,
             name: 'World', 
             route: 'world', 
-            default: true,
             hidden: false
         }),
         Em.Object.create({ 
             active: false,
             name: 'Items', 
             route: 'items', 
-            default: false,
             hidden: false
         }),
     ],
--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,16 +1,3 @@
 export default Ember.Controller.extend({
-    nav: [
-        Em.Object.create({ 
-            active: true,
-            name: 'World', 
-            route: 'world', 
-            default: true
-        }),
-        Em.Object.create({ 
-            active: false,
-            name: 'Items', 
-            route: 'items', 
-            default: false
-        })
-    ],
+
 });
