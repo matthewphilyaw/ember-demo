076b6f657f933cd0f68f3b7c6ed678eb7897c2e8
matthew philyaw <matthew.philyaw@gmail.com>
2014-03-25 07:26:31 -0400

    Modified edit route so that when you navigate directly to an edit url the model is populate from the querystring and edited template to show the value passed in the query strin.

--- app/routes/room/edit.js
+++ app/routes/room/edit.js
@@ -2,12 +2,22 @@ export default Ember.Route.extend({
     beforeModel: function(trans) {
         trans.send('event', 'EDIT');
     },
+    /**
+     * Get's fired when you navigate directly to the url
+     * I'd imagine the link-to helper would fire it to
+     */
     model: function(params) {
-        return null;
+        return Em.Object.create({
+            room_id: params.room_id
+        }); 
     },
+    /**
+     * Fired when .transitionTo is called with a model
+     * like it is in routes/world in the edit action
+     */
     serialize: function(model) {
-        return {
+        return Em.Object.create({
             room_id: model.get('room_id')
-        }
+        });
     }
 });
